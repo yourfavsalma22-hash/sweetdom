4327e31cbf5ca42936d4ca888ba334a6f377fb8e
yourfavsalma22-hash <yourfavsalma22@gmail.com>
2026-07-31 20:44:06 +0300

    Add files via upload

---
+++ sweetdom.js
@@ -0,0 +1,39 @@
+
+// function removeCLASS(ele,remove) {
+//     return ele.classList.remove(remove)
+// }
+// function select(ele){
+//     return document.querySelector(ele)
+// }
+// function selectALL(ele) {
+//    return document.querySelectorAll(ele)
+// }
+// function create(ele,apender) {
+//     let create = document.createElement(ele)
+//      apender.appendChild(create)
+//      return create
+// }
+let seet ="h1"
+ let apend = document.querySelector(".notes")
+ 
+const SweetDOM = {
+    removeCLASS(ele, remove) {
+        return ele.classList.remove(remove);
+    },
+
+    select(ele) {
+        return document.querySelector(ele);
+    },
+
+    selectALL(ele) {
+        return document.querySelectorAll(ele);
+    },
+
+    create(ele,text,apender) {
+        let create1 = document.createElement(ele);
+        create1.textContent = text
+        apender.appendChild(create1);
+        return create1;
+    }
+};
+SweetDOM.create(seet,"ahmed",apend)
